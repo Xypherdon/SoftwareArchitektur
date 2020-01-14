bae2e8d1f0fc6048ce7b7aa992b4b32547795fc1
Filip Rosian <rosian.filip@gmail.com>
2020-01-14 15:32:55 +0200

    Fully functional chat

--- Tinder/app/src/main/java/com/example/tinder/Chat/ChatActivity.java
+++ Tinder/app/src/main/java/com/example/tinder/Chat/ChatActivity.java
@@ -1,6 +1,7 @@
 package com.example.tinder.Chat;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,6 +16,7 @@ import com.example.tinder.Chat.ChatObject;
 import com.example.tinder.MainActivity;
 import com.example.tinder.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -37,17 +39,17 @@ public class ChatActivity extends AppCompatActivity {
 
     private Button mSendButton;
 
-    private String currentUserId, matchId,currentUserGender,currentOppositeGender,chatId;
+    private String currentUserId, matchId, currentUserGender, currentOppositeGender, chatId;
 
-    private DatabaseReference mDataBaseUser,mDatabaseChat;
+    private DatabaseReference mDataBaseUser, mDatabaseChat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        currentUserGender= MainActivity.userSex;
-        currentOppositeGender=MainActivity.oppositeUserSex;
+        currentUserGender = MainActivity.userSex;
+        currentOppositeGender = MainActivity.oppositeUserSex;
 
         matchId = getIntent().getExtras().getString("matchId");
 
@@ -60,7 +62,7 @@ public class ChatActivity extends AppCompatActivity {
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         mRecyclerView.setNestedScrollingEnabled(false);
-        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setHasFixedSize(false);
         mChatLayoutManager = new LinearLayoutManager(ChatActivity.this);
         mRecyclerView.setLayoutManager(mChatLayoutManager);
         mChatAdapter = new ChatAdapter(getDataSetChat(), ChatActivity.this);
@@ -69,7 +71,7 @@ public class ChatActivity extends AppCompatActivity {
         mSendEditText = findViewById(R.id.message);
         mSendButton = findViewById(R.id.send);
 
-        mSendButton.setOnClickListener(new View.OnClickListener(){
+        mSendButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
@@ -81,12 +83,12 @@ public class ChatActivity extends AppCompatActivity {
 
     private void sendMessage() {
         String sendMessageText = mSendEditText.getText().toString();
-        if(!sendMessageText.isEmpty()){
-            DatabaseReference newMessageDb=mDatabaseChat.push();
+        if (!sendMessageText.isEmpty()) {
+            DatabaseReference newMessageDb = mDatabaseChat.push();
 
             Map newMessage = new HashMap();
-            newMessage.put("createdByUser",currentUserId);
-            newMessage.put("text",sendMessageText);
+            newMessage.put("createdByUser", currentUserId);
+            newMessage.put("text", sendMessageText);
 
             newMessageDb.setValue(newMessage);
 
@@ -94,19 +96,65 @@ public class ChatActivity extends AppCompatActivity {
         }
     }
 
-    private void getChatId(){
+    private void getChatId() {
         mDataBaseUser.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists()){
-                    chatId=dataSnapshot.getValue().toString();
-                    mDatabaseChat=mDatabaseChat.child(chatId);
+                if (dataSnapshot.exists()) {
+                    chatId = dataSnapshot.getValue().toString();
+                    mDatabaseChat = mDatabaseChat.child(chatId);
+                    getChatMessages();
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        });
+    }
+
+    private void getChatMessages() {
+        mDatabaseChat.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                if (dataSnapshot.exists()) {
+                    String message = null;
+                    String createdByUser = null;
+                    if(dataSnapshot.child("text").getValue()!=null){
+                        message = dataSnapshot.child("text").getValue().toString();
+                    }
+                    if(dataSnapshot.child("createdByUser").getValue()!=null){
+                        createdByUser = dataSnapshot.child("createdByUser").getValue().toString();
+                    }
+
+                    if(message!=null && createdByUser!=null){
+                        Boolean currentUserBoolean=false;
+                        if(createdByUser.equals(currentUserId)){
+                            currentUserBoolean=true;
+                        }
+
+                        ChatObject newMessage = new ChatObject(message,currentUserBoolean);
+                        resultChat.add(newMessage);
+                        mChatAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
             }
         });
     }
